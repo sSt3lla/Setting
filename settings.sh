24f82b2d6a01b4b6def6da9446db7100c47474a9
(j)Avascript <st3lla_programming@protonmail.com>
2022-12-03 00:14:26 +1300

    Create settings.sh

---
+++ settings.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+sudo apt install wget curl git zsh -yq
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+chsh -s $(which zsh)
+#Install oh my tmux
+sudo apt install tmux xclip -yq
+
+cd
+git clone https://github.com/sSt3lla/.tmux-config
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
+#Now we restart
+sudo reboot
